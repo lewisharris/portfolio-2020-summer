4a13536a01024be186dd5bf9984865a054ea3457
Lewis <lewis@Lewiss-MacBook-Pro.local>
2020-09-14 18:39:12 +0100

    netlify url

---
+++ client/build/precache-manifest.9ff83f9f4aec1c3192dca9e94e1b962a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf8a7ec984352326b660df91ecb3228",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bf7cb0dd0cfed355d07",
+    "url": "/static/css/main.af92d03d.chunk.css"
+  },
+  {
+    "revision": "523f4cabc1adc0a22f17",
+    "url": "/static/js/2.5b46589d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.5b46589d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf7cb0dd0cfed355d07",
+    "url": "/static/js/main.d25557f2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "39057157772d74dffe240537a759d8d7",
+    "url": "/static/media/X.39057157.svg"
+  },
+  {
+    "revision": "7ab96e8e331f6ed6c6208adee23b1e9c",
+    "url": "/static/media/alien-escape.7ab96e8e.jpeg"
+  },
+  {
+    "revision": "f201b4281e703d7c83d6ce74e50031a5",
+    "url": "/static/media/destination-somewhere.f201b428.jpeg"
+  },
+  {
+    "revision": "e2bd4b358edf45920076cd0f60cb794d",
+    "url": "/static/media/dribbble-icon.e2bd4b35.svg"
+  },
+  {
+    "revision": "306434df6fa72082078be5f6c3718862",
+    "url": "/static/media/drum-kit.306434df.jpeg"
+  },
+  {
+    "revision": "a0f10f83d164da8ab5a87d4690e9c173",
+    "url": "/static/media/github-icon.a0f10f83.png"
+  },
+  {
+    "revision": "87351787cab9ce960e77fc774f084514",
+    "url": "/static/media/linkedin-icon.87351787.png"
+  },
+  {
+    "revision": "b15eeb96aa8d4677964295f18c72b7e2",
+    "url": "/static/media/logo-main.b15eeb96.png"
+  },
+  {
+    "revision": "4953bf3a086bd0abf89753b10d21450b",
+    "url": "/static/media/medium-icon.4953bf3a.png"
+  },
+  {
+    "revision": "d9f5f5c68e5aa044737bd0e487bfd095",
+    "url": "/static/media/nutrition-checker.d9f5f5c6.jpeg"
+  },
+  {
+    "revision": "a26b3924a0d094abb22c71ec3f77ea6c",
+    "url": "/static/media/pushfit-mockup.a26b3924.jpg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d26315a50e6330d3d0f4564955265781.js"
+  "/precache-manifest.9ff83f9f4aec1c3192dca9e94e1b962a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
