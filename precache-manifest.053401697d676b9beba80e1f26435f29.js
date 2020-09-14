fadf22c42fcb7ee4af2185e0bba1f86cdac75c61
Lewis <lewis@Lewiss-MacBook-Pro.local>
2020-09-14 17:08:54 +0100

    Updates

---
+++ precache-manifest.053401697d676b9beba80e1f26435f29.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8957e1e7074378fbcd536d4d0acbfc",
+    "url": "/portfolio-2020-summer/index.html"
+  },
+  {
+    "revision": "1bf7cb0dd0cfed355d07",
+    "url": "/portfolio-2020-summer/static/css/main.af92d03d.chunk.css"
+  },
+  {
+    "revision": "931ecac83a1bf314ed98",
+    "url": "/portfolio-2020-summer/static/js/2.4b0de750.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio-2020-summer/static/js/2.4b0de750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf7cb0dd0cfed355d07",
+    "url": "/portfolio-2020-summer/static/js/main.d25557f2.chunk.js"
+  },
+  {
+    "revision": "d6ec680e710da8650397",
+    "url": "/portfolio-2020-summer/static/js/runtime-main.0a487775.js"
+  },
+  {
+    "revision": "39057157772d74dffe240537a759d8d7",
+    "url": "/portfolio-2020-summer/static/media/X.39057157.svg"
+  },
+  {
+    "revision": "7ab96e8e331f6ed6c6208adee23b1e9c",
+    "url": "/portfolio-2020-summer/static/media/alien-escape.7ab96e8e.jpeg"
+  },
+  {
+    "revision": "f201b4281e703d7c83d6ce74e50031a5",
+    "url": "/portfolio-2020-summer/static/media/destination-somewhere.f201b428.jpeg"
+  },
+  {
+    "revision": "e2bd4b358edf45920076cd0f60cb794d",
+    "url": "/portfolio-2020-summer/static/media/dribbble-icon.e2bd4b35.svg"
+  },
+  {
+    "revision": "306434df6fa72082078be5f6c3718862",
+    "url": "/portfolio-2020-summer/static/media/drum-kit.306434df.jpeg"
+  },
+  {
+    "revision": "a0f10f83d164da8ab5a87d4690e9c173",
+    "url": "/portfolio-2020-summer/static/media/github-icon.a0f10f83.png"
+  },
+  {
+    "revision": "87351787cab9ce960e77fc774f084514",
+    "url": "/portfolio-2020-summer/static/media/linkedin-icon.87351787.png"
+  },
+  {
+    "revision": "b15eeb96aa8d4677964295f18c72b7e2",
+    "url": "/portfolio-2020-summer/static/media/logo-main.b15eeb96.png"
+  },
+  {
+    "revision": "4953bf3a086bd0abf89753b10d21450b",
+    "url": "/portfolio-2020-summer/static/media/medium-icon.4953bf3a.png"
+  },
+  {
+    "revision": "d9f5f5c68e5aa044737bd0e487bfd095",
+    "url": "/portfolio-2020-summer/static/media/nutrition-checker.d9f5f5c6.jpeg"
+  },
+  {
+    "revision": "a26b3924a0d094abb22c71ec3f77ea6c",
+    "url": "/portfolio-2020-summer/static/media/pushfit-mockup.a26b3924.jpg"
+  }
+]);
